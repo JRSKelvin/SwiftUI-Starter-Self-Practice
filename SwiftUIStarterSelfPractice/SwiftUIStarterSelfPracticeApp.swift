30b3693849b56511fc987ce8a252ac945833d59c
Kelvin <74870436+JRSKelvin@users.noreply.github.com>
2024-02-05 20:26:15 -0700

    Initial Commit

---
+++ SwiftUIStarterSelfPractice/SwiftUIStarterSelfPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStarterSelfPracticeApp.swift
+//  SwiftUIStarterSelfPractice
+//
+//  Created by xc39b2 on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStarterSelfPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
